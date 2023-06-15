a30a93ad544bbe44affa756baa267e98a2c3f6ed
suruchi sonone <suruchisonone6@gmail.com>
2023-06-15 15:56:19 +0530

    5th

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useRef,useState} from "react";
 
 
 function App(){
@@ -10,19 +10,20 @@ function App(){
 }
 
 function ListDemo(){
+  let inputref=useRef();
   let [list,setList]=useState(["delhi"]);
 
   let addlist=()=>{
-    let inputref=document.querySelector("#id1");
-    let inputvalue=inputref.value;
+    // let inputref=document.querySelector("#id1");
+    let inputvalue=inputref.current.value;
     let newlist=[...list,inputvalue];
     setList(newlist);
-    inputref.value="";
+    inputref.current.value="";
   };
 
   return(
     <>
-    <input type="text" id="id1" placeholder="enter the value"/>
+    <input type="text" id="id1" ref={inputref} placeholder="enter the value"/>
     <input type="button" value="add new list" onClick={addlist}/>
 
     {list.map((item)=>(<h1>{item}</h1>))}
